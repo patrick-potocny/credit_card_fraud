d4957c3b12103f7ae853433a7b119b7445aac538
Pato <patrik.potocny@gmail.com>
2021-01-14 08:53:35 +0100

    Used package lazypredict to tet all sklearn classification models on unsampled data to use as baseline.

---
+++ usefull_funcs.py
@@ -0,0 +1,35 @@
+"""
+This file is set od random usefull functions i use thruout most of my projects
+"""
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+a4_dims = (11.7, 8.27)
+
+def hist_for_loop(df):
+    for col in df.columns:
+        fig, ax = plt.subplots(figsize=a4_dims)
+        sns.histplot(df[col])
+        plt.title(col)
+        plt.show()
+
+
+def box_plot_for_loop(df):
+    for col in df.columns:
+        fig, ax = plt.subplots(figsize=a4_dims)
+        sns.boxplot(df[col])
+        plt.title(col)
+        plt.show()
+
+
+def outlier_treatment(datacolumn, iqr_multiplier):
+    sorted(datacolumn)
+    q1, q3 = np.percentile(datacolumn, [25, 75])
+    iqr = q3 - q1
+    lower_range = q1 - (iqr_multiplier * iqr)
+    upper_range = q3 + (iqr_multiplier * iqr)
+    return lower_range, upper_range
+
+
